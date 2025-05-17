cae04a05f2dc3e04fa60574fc8733970bf212be7
Kingapplefruit <84571863+Kingapplefruit@users.noreply.github.com>
2025-05-17 23:49:04 +0530

    PORT fix

--- backend_demo_website/index.js
+++ backend_demo_website/index.js
@@ -43,6 +43,6 @@ app.post('/api/save-text', async (req, res) => {
 });
 
 // Start the server listening on port 3000
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
